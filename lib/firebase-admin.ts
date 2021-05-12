8db51e321b255e31c2711fc0eba1b43359e849a6
Jithin Antony <jithinantony333@gmail.com>
2021-05-12 22:08:39 +0530

    updated firebase-admin.ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -7,7 +7,7 @@ export async function initFirebaseAdmin() {
                     type: process.env.NEXT_FIREBASE_PRIVATE_TYPE,
                     project_id: process.env.NEXT_FIREBASE_PRIVATE_PROJECT_ID,
                     private_key_id: process.env.NEXT_FIREBASE_PRIVATE_PRIVATE_KEY_ID,
-                    private_key: process.env.NEXT_FIREBASE_PRIVATE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+                    private_key: process.env.NEXT_FIREBASE_PRIVATE_PRIVATE_KEY,
                     client_email: process.env.NEXT_FIREBASE_PRIVATE_CLIENT_EMAIL,
                     client_id: process.env.NEXT_FIREBASE_PRIVATE_CLIENT_ID,
                     auth_uri: process.env.NEXT_FIREBASE_PRIVATE_AUTH_URI,
