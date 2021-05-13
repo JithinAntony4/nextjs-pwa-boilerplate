dc50289c89971de0cfd499dc3d887e245c897dbe
Jithin Antony <jithinantony333@gmail.com>
2021-05-13 14:43:55 +0530

    rename env firebase variables

--- webPush.js
+++ webPush.js
@@ -12,13 +12,13 @@ const firebaseCloudMessaging = {
     init: async function () {
         if (!firebase.apps.length) {
             firebase.initializeApp({
-                apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-                authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-                projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-                storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-                messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-                appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-                measurementId: process.env.NEXT_PUBLIC_FIREBASE_MESUREMENT_ID
+                apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
+                authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+                projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+                storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+                messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+                appId: process.env.PUBLIC_FIREBASE_APP_ID,
+                measurementId: process.env.PUBLIC_FIREBASE_MESUREMENT_ID
             });
             try {
                 const messaging = firebase.messaging();
